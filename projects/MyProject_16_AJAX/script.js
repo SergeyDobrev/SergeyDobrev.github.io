785b4de45206e711f23c5e6d5a4ac19849fe260b
SergeyDobrev <dobrev.sergey@gmail.com>
2018-04-03 14:08:15 +0300

    51th commit

--- projects/MyProject_16_AJAX/script.js
+++ projects/MyProject_16_AJAX/script.js
@@ -1,17 +1,30 @@
 (function() {
-	var objAjax;
 	var xhr = new XMLHttpRequest(); 
 	xhr.open("GET","ajax.txt",true);
 	xhr.send();
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState === 4 && xhr.status === 200) {
 			var obj = JSON.parse(xhr.responseText);
-			console.log(obj.length);
+
 			for (var i = 0; i < obj.length; i++) {
-				var tmp = document.createElement(obj[i].tagVar);
-				$(obj[i].parrentVar).append(tmp);
-				$(tmp).attr(obj[i].attrVar);	
-				$(tmp).text(obj[i].textVar);
+
+				var tmp = document.createElement(obj[i].tagVal);
+				$(obj[i].parrentVal).append(tmp);
+				$(tmp).attr(obj[i].attrVal);
+
+				if (obj[i].textVal != undefined ) $(tmp).text(obj[i].textVal);
+
+				if (obj[i].attrVal.class === 'skillItemName') {
+
+					for (var j = 0; j < 10; j++) {
+
+						var skl = document.createElement('div');
+						$(obj[i].parrentVal).append(skl);
+
+						if (j < obj[i].levelVal) $(skl).addClass('skillLevel green');
+						else $(skl).addClass('skillLevel');
+					}
+				}
 			}
 		}
 	}
