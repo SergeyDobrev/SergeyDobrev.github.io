e838b0956bca0160a27647c23e787cbb66d3e808
SergeyDobrev <dobrev.sergey@gmail.com>
2018-05-28 11:36:35 +0300

    73th commit

--- projects/MyProject_18_ps/script.js
+++ projects/MyProject_18_ps/script.js
@@ -21,26 +21,26 @@ for (var i = 0; i < 7; i++) {
 		myObjArray[i][j].style.left = leftVar + (j * w/10) + 'px';
 		myObjArray[i][j].style.top = topVar + (i * w/10) + 'px';
 		myObjArray[i][j].style.transition = 'transform 1s';
+		myObjArray[i][j].style.transform = 'rotateY(90deg) rotateX(90deg)';
 		myObjArray[i][j].style.backgroundImage = 'url(fon_2.jpg)';		
 		myObjArray[i][j].style.backgroundSize = w * 2 + 'px ' + w * 0.7 + 'px';		
 		myObjArray[i][j].style.backgroundPosition = (w * 2 - j * w / 10) + 'px ' + (w * 0.7 - i * w / 10) + 'px';
 
-		myObjArray[i][j].onmousemove = function() {
-
-				this.style.transform = 'rotateY(360deg)';
-
-		}
 	}
 }
 
-// var t = 0;
-// for (var i = 0; i < 7; i++) {
-// 	for (var j = 0; j < 20; j++) {
-// 		t = t + 500;
-// 		var timerId = setTimeout(myObjArray[i][j].moveFon(),t);
-// 		console.log(t);
-		
-// 	}
-// }
+var i = 6;
+var j = 0;
+var n = 0;
+var IntervalId = setInterval(
+	function() {
+		myObjArray[i--][j++].style.transform = 'rotateY(0deg) rotateX(0deg)';
+		if (n++ === 140) clearInterval(IntervalId);
+		if (i === -1) i = 6;
+		if (j === 20) j = 0;
+			
+
+	},5);
+
 
 
